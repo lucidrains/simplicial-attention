88d18be29b36d2a0b400dbf9dfff83b34ad302e6
lucidrains <lucidrains@gmail.com>
2025-07-06 08:25:11 -0700

    generalize

--- simplicial_attention/simplicial_attention.py
+++ simplicial_attention/simplicial_attention.py
@@ -4,6 +4,11 @@ from torch import nn
 from einops import einsum, rearrange, pack, unpack
 from opt_einsum import contract
 
+# functions
+
+def join(arr, delimiter = ', '):
+    return delimiter.join(arr)
+
 # 2-simplicial attention
 
 def naive_two_simplicial_attend(
@@ -27,5 +32,48 @@ def naive_two_simplicial_attend(
     attn, = unpack(packed_attn, packed_shape, 'b h i *')
 
     out = contract('... i j k, ... j d, ... k d -> ... i d', attn, v1, v2)
+
+    return out
+
+# n-th order attention, for good measure
+
+def nth_order_attend(
+    q,     # b h i d
+    keys,  # list[b h jkl... d]
+    values # list[b h jkl... dv]
+):  # b h i dv 
+
+    assert len(keys) == len(values)
+    n = len(keys)
+
+    scale = q.shape[-1] * -0.5
+
+    q = q * scale
+
+    # construct equations
+
+    start_index = ord('j')
+
+    ord_indices = list(range(start_index, start_index + n))
+
+    similarity_lfs_eq = join([f'... {chr(i)} d' for i in ord_indices], ', ')
+
+    similarity_rhs_eq = join([chr(i) for i in ord_indices],  ' ')
+
+    similarity_ein_equation = f'... i d, {similarity_lfs_eq} -> ... i {similarity_rhs_eq}'
+
+    aggregate_ein_equation = f'... i {similarity_rhs_eq}, {similarity_lfs_eq} -> ... i d'
+
+    # nth order attention
+
+    sim = einsum(q, *keys, similarity_ein_equation)
+
+    packed_sim, packed_shape = pack((sim,), 'b h i *')
+
+    packed_attn = packed_sim.softmax(dim = -1)
+
+    attn, = unpack(packed_attn, packed_shape, 'b h i *')
+
+    out = einsum(attn, *values, aggregate_ein_equation)
 
     return out
--- tests/test_attn.py
+++ tests/test_attn.py
@@ -17,3 +17,18 @@ def test_attn():
     )
 
     assert attended.shape == q.shape
+
+def test_fifth_order():
+    from simplicial_attention import nth_order_attend
+
+    q = torch.randn(1, 8, 4, 16)
+    k = torch.randn(1, 8, 4, 16)
+    v = torch.randn(1, 8, 4, 16)
+
+    fifth_order_attended = nth_order_attend(
+        q,
+        [k, k, k, k, k],
+        [v, v, v, v, v]
+    )
+
+    assert fifth_order_attended.shape == q.shape
