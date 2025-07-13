found a bug in their pseudocode forward kernel.. correct it and make sure forward works for both causal and non-causal

test_triton.py
@@ -23,7 +23,7 @@ except ImportError:
 
 assert exists(flex_attention)
 
-def create_tilinear_sliding_mask(seq_len, window1_size, window2_size):
+def create_tilinear_sliding_mask(seq_len, window1_size, window2_size, causal):
 
     def sliding_mask(_, __, q_index, kv_index):
 
@@ -33,18 +33,19 @@ def create_tilinear_sliding_mask(seq_len, window1_size, window2_size):
         distance1 = q_index - kv1_index
         distance2 = q_index - kv2_index
 
-        backward_sliding_mask1 = distance1 <= window1_size
-        backward_sliding_mask2 = distance2 <= window2_size
+        backward_sliding_mask1 = distance1 < window1_size
+        backward_sliding_mask2 = distance2 < window2_size
 
         forward_sliding_mask1 = distance1 >= 0
         forward_sliding_mask2 = distance2 >= 0
 
         return backward_sliding_mask1 & backward_sliding_mask2 & forward_sliding_mask1 & forward_sliding_mask2
 
-    block_mask = create_block_mask(sliding_mask, B = None, H = None, Q_LEN = seq_len, KV_LEN = seq_len * seq_len, _compile = True)
+    block_mask = create_block_mask(sliding_mask, B = None, H = None, Q_LEN = seq_len, KV_LEN = seq_len * seq_len, _compile = True) if causal else None
+
     return block_mask
 
-def flex_sliding_two_simplicial_attn(q, k1, k2, v1, v2, w1 = 64, w2 = 32):
+def flex_sliding_two_simplicial_attn(q, k1, k2, v1, v2, w1 = 64, w2 = 32, causal = True):
     q, k1, k2, v1, v2 = tuple(rearrange(t, 'b n h d -> b h n d') for t in (q, k1, k2, v1, v2))
 
     seq_len = q.shape[-2]
@@ -53,18 +54,21 @@ def flex_sliding_two_simplicial_attn(q, k1, k2, v1, v2, w1 = 64, w2 = 32):
     v = einsum(v1, v2, 'b h i d, b h j d -> b h i j d')
     k, v = tuple(rearrange(t, 'b h i j d -> b h (i j) d') for t in (k, v))
 
-    block_mask = create_tilinear_sliding_mask(seq_len, w1, w2)
+    block_mask = create_tilinear_sliding_mask(seq_len, w1, w2, causal)
 
     out = flex_attention(q, k, v, block_mask = block_mask, enable_gqa = True)
     return rearrange(out, 'b h n d -> b n h d')
 
 # queries, keys, values
 
-q = torch.randn(2, 1024, 4, 64).cuda()
-k1 = torch.randn(2, 1024, 4, 64).cuda()
-k2 = torch.randn(2, 1024, 4, 64).cuda()
-v1 = torch.randn(2, 1024, 4, 64).cuda()
-v2 = torch.randn(2, 1024, 4, 64).cuda()
+causal = True
+seq_len = 64
+
+q = torch.randn(2, seq_len, 4, 64).cuda()
+k1 = torch.randn(2, seq_len, 4, 64).cuda()
+k2 = torch.randn(2, seq_len, 4, 64).cuda()
+v1 = torch.randn(2, seq_len, 4, 64).cuda()
+v2 = torch.randn(2, seq_len, 4, 64).cuda()
 
 tq = q.detach().clone()
 tk1 = k1.detach().clone()
@@ -79,7 +83,7 @@ w2 = 32
 
 # inefficient way
 
-flex_forward_out = flex_sliding_two_simplicial_attn(q, k1, k2, v1, v2, w1 = w1, w2 = w2)
+flex_forward_out = flex_sliding_two_simplicial_attn(q, k1, k2, v1, v2, w1 = w1, w2 = w2, causal = causal)
 
 # triton kernel
 
@@ -88,7 +92,8 @@ triton_forward_out = sliding_two_simplicial_attn(
     (tk1, tk2),
     (tv1, tv2),
     w1 = w1,
-    w2 = w2
+    w2 = w2,
+    causal = causal
 )
 
 # asserts